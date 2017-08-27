a237377978fc275ba4af5f4f80b34e4082d23249
Luis Molina <luismol@umich.edu>
2017-08-27 16:04:09 -0400

    Splitter contract done

---
+++ splitter/contracts/Splitter.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.6;
+
+contract Splitter {
+
+    mapping (address => Split) splitMetadata;
+    mapping (address => bool) participants;
+
+    struct Split {
+        address beneficiaryOne;
+        address beneficiaryTwo;
+    }
+
+	event SplitRegistered(address from, address one, address two);
+	event EtherSplit(address from, address one, address two, uint amount);
+
+	function registerSplit(address beneficiaryOne, address beneficiaryTwo) {
+
+	    Split memory split;
+        split.beneficiaryOne = beneficiaryOne;
+        split.beneficiaryTwo = beneficiaryTwo;
+
+	    splitMetadata[msg.sender] = split;
+	    participants[msg.sender] = true;
+
+	    SplitRegistered(msg.sender, beneficiaryOne, beneficiaryTwo);
+	}
+
+	function () payable {
+	    require(participants[msg.sender]);
+	    require(msg.value > 0);
+
+	    uint amount = msg.value / 2;
+	    Split memory split = splitMetadata[msg.sender];
+
+	    split.beneficiaryOne.transfer(amount);
+	    split.beneficiaryTwo.transfer(amount);
+
+	    EtherSplit(msg.sender, split.beneficiaryOne, split.beneficiaryTwo,
+	        amount);
+	}
+}
